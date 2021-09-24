8ee51e98e2338e321324f6b8b4c3cd2ca6cbcdd9
Jeremi Piotrowski <jpiotrowski@microsoft.com>
2021-09-24 21:55:31 +0200

    fix syscall numbers for arm
    The previous numbers were not correct at all, and resulted in SIGILL being
    raised when called on ARM. I took the updated values from:
    https://chromium.googlesource.com/chromiumos/docs/+/master/constants/syscalls.md

--- sys_linux_arm.go
+++ sys_linux_arm.go
@@ -1,7 +1,7 @@
 package keyctl
 
 const (
-	syscall_keyctl   uintptr = 9437495
-	syscall_add_key  uintptr = 9437493
-	syscall_setfsgid uintptr = 9437323
+	syscall_keyctl   uintptr = 311
+	syscall_add_key  uintptr = 309
+	syscall_setfsgid uintptr = 139
 )
